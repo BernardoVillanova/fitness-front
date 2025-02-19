b3ab57d9cd43489ed4c192db610b4d12999beeb6
BernardoYeb <bernardo.vilanova@yeb.com.br>
2025-02-19 07:16:31 -0300

    :sparkles: | first commit / jwt, swagger instanciado

---
+++ src/views/login.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <h2>Login</h2>
+      <input v-model="email" placeholder="Email">
+      <input v-model="password" type="password" placeholder="Senha">
+      <button @click="login">Entrar</button>
+    </div>
+  </template>
+  
+  <script>
+  import api from "@/api"; // Arquivo que configura axios
+  
+  export default {
+    data() {
+      return { email: "", password: "" };
+    },
+    methods: {
+      async login() {
+        try {
+          const response = await api.post("/auth/login", { email: this.email, password: this.password });
+          localStorage.setItem("token", response.data.token);
+          alert("Login bem-sucedido!");
+        } catch (error) {
+          alert("Erro no login");
+        }
+      }
+    }
+  };
+  </script>
+  
