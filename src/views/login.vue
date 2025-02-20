3859d49325cc2d13d8b06d1c5cec549e62f840bb
BernardoYeb <bernardo.vilanova@yeb.com.br>
2025-02-20 20:46:46 -0300

    :sparkles: Comunicação entre rotas funcional

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,9 +1,10 @@
 <template>
     <div>
-      <h2>Login</h2>
+      <h2>Tela login</h2>
       <input v-model="email" placeholder="Email">
       <input v-model="password" type="password" placeholder="Senha">
       <button @click="login">Entrar</button>
+      <router-link to="/">Voltar para Home</router-link>
     </div>
   </template>
   
@@ -11,6 +12,7 @@
   import api from "@/api"; // Arquivo que configura axios
   
   export default {
+    name: 'LoginPage',
     data() {
       return { email: "", password: "" };
     },
