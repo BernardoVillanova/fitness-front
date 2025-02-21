3d72b825d0d535757937a3eea41ddda1f388f1c1
BernardoYeb <bernardo.vilanova@yeb.com.br>
2025-02-21 07:08:33 -0300

    :sparkles: Comunicação entre telas

---
+++ src/views/home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>Tela Home</h1>
+      <router-link to="/login">Voltar para Login</router-link>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "HomePage",
+  };
+  </script>
