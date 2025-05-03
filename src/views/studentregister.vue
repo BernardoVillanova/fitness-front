2c21ca491439c06229110fca8424a69a3726fa82
BernardoVillanova <bernardo.villasantana@gmail.com>
2025-05-03 10:08:06 -0300

    DEV-002 | :sparkles: Correção de Arquivos

--- src/views/studentregister.vue
+++ src/views/studentregister.vue
@@ -1,2 +1,14 @@
 <!-- Será necessário pegar o userId do preenchimento feito e após isto prosseguir com este  -->
- <h1>Para Depois XDD</h1>
+<template>
+    <h2>Formulário com etapas para criação de Aluno</h2>
+</template>
+
+<script>
+ export default {
+    name: 'StudentRegister',
+ }
+</script>
+
+<style>
+
+</style>
